fb6341b7ade471a5e23c3eccb0910ac0fa665d20
Erich Niemand <erichniem@gmail.com>
2023-05-09 13:54:03 +0200

    Working modal, need to save data

--- MovieApp/ClientApp/src/app/movie/movie.component.ts
+++ MovieApp/ClientApp/src/app/movie/movie.component.ts
@@ -1,19 +1,38 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+declare var window: any;
 
 @Component({
   selector: 'movie',
   templateUrl: './movie.component.html',
 })
 
-export class MovieComponent {
+export class MovieComponent implements OnInit {
   public movies: Movie[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Movie[]>(baseUrl + 'movie').subscribe(result => {
       this.movies = result;
     }, error => console.error(error));
+  };
+
+  formModal: any;
+
+  ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(document.getElementById("addMovie"));
+  }
+
+  addMovie() {
+    this.formModal.show();
+  }
+
+  saveMovie() {
+    this.formModal.hide();
+  }
+
+  closeNoSave() {
+    this.formModal.hide();
   }
 
 }
